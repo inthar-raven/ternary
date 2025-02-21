fa104eb0008f394db5aad391a26496e02e8ff777
inthar-raven <serotoceprafen@gmail.com>
2025-02-21 07:42:20 -0500

    Update to Rust 2024 edition

--- src/lib.rs
+++ src/lib.rs
@@ -174,10 +174,7 @@ fn numbers_to_string(word: &[usize]) -> String {
 }
 
 fn guide_frame_to_result(structure: &GuideFrame) -> GuideResult {
-    let GuideFrame {
-        ref gs,
-        ref polyoffset,
-    } = structure;
+    let GuideFrame { gs, polyoffset } = structure;
     if structure.multiplicity() == 1 {
         GuideResult {
             gs: gs
