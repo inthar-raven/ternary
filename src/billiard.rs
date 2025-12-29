807c294aa45f4fd0802871d3c58a07b1e03f4146
inthar-raven <serotoceprafen@gmail.com>
2025-12-29 08:40:09 -0500

    Clean up billiard code

--- src/billiard.rs
+++ src/billiard.rs
@@ -1,6 +1,218 @@
+use core::fmt;
+
+use num_rational::Rational32 as r32;
+
 use crate::helpers::gcd;
-use crate::plane_geometry::{ConvexPolygon, advance, project_and_partition};
+use crate::plane_geometry::{ConvexPolygon, Line, Point, PointLineConfiguration, Slope};
 use crate::words::{Letter, least_mode};
+
+// ERRORS
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+/// Error type for illegal billiard trajectory states.
+pub enum BadBilliardState {
+    // Point is not inside the projected cube,
+    PointNotInCube(Point),
+    // Point is inside the projected cube but falls on a projected constraint plane
+    PointOnConstraintPlane(Point),
+}
+
+impl fmt::Display for BadBilliardState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::PointNotInCube(pt) => write!(f, "Point {} is not inside the projected cube", pt),
+            Self::PointOnConstraintPlane(pt) => write!(
+                f,
+                "Point {} is inside the projected cube but falls on a projected constraint plane",
+                pt
+            ),
+        }
+    }
+}
+
+impl std::error::Error for BadBilliardState {}
+
+fn projected_constraint_planes(a: u32, b: u32, c: u32) -> (Vec<Line>, Vec<Line>, Vec<Line>) {
+    let first = (0..b + c + 1)
+        .map(|i| {
+            Line::from_slope_and_point(
+                Slope::raw(0, 1),
+                Point::new(r32::new(0, 1), r32::new(-(b as i32) + (i as i32), c as i32)),
+            )
+        })
+        .collect(); // horizontal lines going up (Left)
+    let second = (0..a + c + 1)
+        .map(|j| {
+            Line::from_slope_and_point(
+                Slope::raw(1, 0),
+                Point::new(r32::new((c as i32) - (j as i32), c as i32), r32::new(0, 1)),
+            )
+        })
+        .collect(); // vertical lines going left (Left)
+    let third = (0..a + b + 1)
+        .map(|k| {
+            Line::from_slope_and_point(
+                Slope::raw(b as i32, a as i32),
+                Point::new(r32::new(0, 1), r32::new(-(b as i32) + (k as i32), a as i32)),
+            )
+        })
+        .collect(); // lines of pos. slope going up (Left)
+    (first, second, third)
+}
+
+/// Project the unit cube in R^3 via a linear map that has (a,b,c) in its kernel.
+fn projected_cube(a: u32, b: u32, c: u32) -> Option<ConvexPolygon> {
+    if a > 0 && b > 0 && c > 0 {
+        let projected_cube_vertices = vec![
+            Point::new(r32::new(0, 1), r32::new(1, 1)),
+            Point::new(r32::new(1, 1), r32::new(1, 1)),
+            Point::new(r32::new(1, 1), r32::new(0, 1)),
+            Point::new(
+                r32::new(-(a as i32), c as i32),
+                r32::new(-(b as i32), c as i32) + r32::new(1, 1),
+            ),
+            Point::new(
+                r32::new(-(a as i32), c as i32),
+                r32::new(-(b as i32), c as i32),
+            ),
+            Point::new(
+                r32::new(-(a as i32), c as i32) + r32::new(1, 1),
+                r32::new(-(b as i32), c as i32),
+            ),
+        ];
+        ConvexPolygon::new(projected_cube_vertices)
+    } else {
+        None
+    }
+}
+
+/// Partition the projected unit cube with the projected constraint planes.
+fn project_and_partition(a: u32, b: u32, c: u32) -> Vec<ConvexPolygon> {
+    let projected_cube = projected_cube(a, b, c).unwrap();
+    let lineses = projected_constraint_planes(a, b, c); // it's a collection of collections of `Line`s
+    let mut first_shapes = Vec::<ConvexPolygon>::new();
+    let mut second_shapes = Vec::<ConvexPolygon>::new();
+    let mut third_shapes = Vec::<ConvexPolygon>::new();
+    let mut i: usize = 0;
+    let mut remaining: ConvexPolygon = projected_cube;
+    loop {
+        let (maybe_remaining, maybe_next) = remaining.subdivide((lineses.0)[i]);
+        // The remaining portion is to the Left.
+        match (maybe_remaining, maybe_next) {
+            (Some(r), None) => {
+                remaining = r;
+            }
+            (Some(r), Some(next)) => {
+                remaining = r;
+                first_shapes.push(next);
+            }
+            (None, Some(next)) => {
+                // This is the last `next` you need to look at.
+                first_shapes.push(next);
+                break;
+            }
+            (_, _) => {
+                break;
+            }
+        }
+        i += 1;
+    }
+    for polygon in first_shapes {
+        let mut j: usize = 0;
+        let mut remaining2: ConvexPolygon = polygon;
+        loop {
+            let (maybe_remaining, maybe_next) = remaining2.subdivide((lineses.1)[j]); // The remaining portion is to the Left.
+            match (maybe_remaining, maybe_next) {
+                (Some(r), None) => {
+                    remaining2 = r;
+                }
+                (Some(r), Some(next)) => {
+                    remaining2 = r;
+                    second_shapes.push(next);
+                }
+                (None, Some(next)) => {
+                    second_shapes.push(next);
+                    break;
+                }
+                (_, _) => {
+                    break;
+                }
+            }
+            j += 1;
+        }
+    }
+    for polygon in second_shapes {
+        let mut j: usize = 0;
+        let mut remaining3: ConvexPolygon = polygon;
+        loop {
+            let (maybe_remaining, maybe_next) = remaining3.subdivide((lineses.2)[j]);
+            // The remaining portion is to the Left.
+            match (maybe_remaining, maybe_next) {
+                (Some(r), None) => {
+                    remaining3 = r;
+                }
+                (Some(r), Some(next)) => {
+                    remaining3 = r;
+                    third_shapes.push(next);
+                }
+                (None, Some(next)) => {
+                    third_shapes.push(next);
+                    break;
+                }
+                (_, _) => {
+                    break;
+                }
+            }
+            j += 1;
+        }
+    }
+    third_shapes
+}
+
+/// Advance a projected point, `pt`, by one letter according to what integer coordinate it next hits as it traverses in the given velocity,
+/// and return the next point and the corresponding letter, one of "0", "1", and "2".
+fn advance(a: u32, b: u32, c: u32, pt: Point) -> Result<(Point, Letter), BadBilliardState> {
+    let projected_cube = projected_cube(a, b, c).unwrap();
+    // Consider: _ /
+    //            |
+    if !projected_cube.has_point_inside(pt) {
+        Err(BadBilliardState::PointNotInCube(pt))
+    } else {
+        let projected_x_y_equals_1_1 = Line::from_slope_and_point(
+            Slope::raw(b as i32, a as i32),
+            Point::new(r32::new(0, 1), r32::new(-(b as i32) + (a as i32), a as i32)),
+        ); // the /
+        let projected_x_z_equals_1_1 = Line::from_slope_and_point(
+            Slope::raw(0, 1),
+            Point::new(r32::new(0, 1), r32::new(-(b as i32) + (c as i32), c as i32)),
+        ); // the _, oriented right
+        let projected_y_z_equals_1_1 = Line::from_slope_and_point(
+            Slope::raw(1, 0),
+            Point::new(r32::new((c as i32) - (a as i32), c as i32), r32::new(0, 1)),
+        ); // the |, oriented up
+        if projected_x_y_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
+            && projected_y_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
+        {
+            // Below the / and to the right of the | => x has changed, change x coordinate to 0 and project; move x coordinate to the left
+            Ok((pt - Point::new(r32::new(1, 1), r32::new(0, 1)), 0)) // new point is one unit to the left
+        } else if projected_x_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
+            && projected_x_y_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
+        {
+            // Above the _ and to the left of the / => y has changed, change y coordinate to 0 and project; move y coordinate down
+            Ok((pt - Point::new(r32::new(0, 1), r32::new(1, 1)), 1)) // new point is one unit below
+        } else if projected_x_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
+            && projected_y_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
+        {
+            // Below the _ and to the left of the | => z has changed, change y coordinate to 0 and project
+            Ok((
+                pt + Point::new(r32::new(a as i32, c as i32), r32::new(b as i32, c as i32)),
+                2,
+            )) // new point is one unit below in the z-direction, and e3 is projected to (-a/c, -b/c)
+        } else {
+            Err(BadBilliardState::PointOnConstraintPlane(pt))
+        }
+    }
+}
 
 /// Billiard scales with the given signature up to rotation.
 pub fn billiard_scales(a: usize, b: usize, c: usize) -> Vec<Vec<Letter>> {
--- src/plane_geometry.rs
+++ src/plane_geometry.rs
@@ -10,7 +10,6 @@ use std::ops::Sub;
 use num_rational::Rational32 as r32;
 
 use crate::helpers::gcd;
-use crate::words::Letter;
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 /// The possible configurations of a point and an oriented line on the plane.
@@ -20,7 +19,7 @@ pub enum PointLineConfiguration {
     OnTheLine, // The point is on the line.
 }
 
-fn is_between(a: r32, b: r32, c: r32) -> bool {
+fn is_between_r32(a: r32, b: r32, c: r32) -> bool {
     if b == c {
         a == b
     } else if b < c {
@@ -119,7 +118,7 @@ impl Slope {
 }
 
 /// A finite point; a member of Q^2.
-#[derive(Copy, Clone, Debug, Hash, PartialEq)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub struct Point {
     // The x-coordinate.
     x: r32,
@@ -454,8 +453,8 @@ impl ConvexPolygon {
             // Look for intersection points on edges with the line.
             for edge in &self.lines_for_edges() {
                 if let Some(intsn) = intersection(*edge, line)
-                    && is_between(intsn.x, edge.point1.x, edge.point2.x)
-                    && is_between(intsn.y, edge.point1.y, edge.point2.y)
+                    && is_between_r32(intsn.x, edge.point1.x, edge.point2.x)
+                    && is_between_r32(intsn.y, edge.point1.y, edge.point2.y)
                 {
                     vertices_left.push(intsn);
                     vertices_right.push(intsn);
@@ -467,193 +466,5 @@ impl ConvexPolygon {
             ConvexPolygon::new(vertices_left),
             ConvexPolygon::new(vertices_right),
         )
-    }
-}
-
-fn projected_constraint_planes(a: u32, b: u32, c: u32) -> (Vec<Line>, Vec<Line>, Vec<Line>) {
-    let first = (0..b + c + 1)
-        .map(|i| {
-            Line::from_slope_and_point(
-                Slope::raw(0, 1),
-                Point::new(r32::new(0, 1), r32::new(-(b as i32) + (i as i32), c as i32)),
-            )
-        })
-        .collect(); // horizontal lines going up (Left)
-    let second = (0..a + c + 1)
-        .map(|j| {
-            Line::from_slope_and_point(
-                Slope::raw(1, 0),
-                Point::new(r32::new((c as i32) - (j as i32), c as i32), r32::new(0, 1)),
-            )
-        })
-        .collect(); // vertical lines going left (Left)
-    let third = (0..a + b + 1)
-        .map(|k| {
-            Line::from_slope_and_point(
-                Slope::raw(b as i32, a as i32),
-                Point::new(r32::new(0, 1), r32::new(-(b as i32) + (k as i32), a as i32)),
-            )
-        })
-        .collect(); // lines of pos. slope going up (Left)
-    (first, second, third)
-}
-
-/// Project the unit cube in R^3 via a linear map that has (a,b,c) in its kernel.
-pub fn projected_cube(a: u32, b: u32, c: u32) -> Option<ConvexPolygon> {
-    if a > 0 && b > 0 && c > 0 {
-        let projected_cube_vertices = vec![
-            Point::new(r32::new(0, 1), r32::new(1, 1)),
-            Point::new(r32::new(1, 1), r32::new(1, 1)),
-            Point::new(r32::new(1, 1), r32::new(0, 1)),
-            Point::new(
-                r32::new(-(a as i32), c as i32),
-                r32::new(-(b as i32), c as i32) + r32::new(1, 1),
-            ),
-            Point::new(
-                r32::new(-(a as i32), c as i32),
-                r32::new(-(b as i32), c as i32),
-            ),
-            Point::new(
-                r32::new(-(a as i32), c as i32) + r32::new(1, 1),
-                r32::new(-(b as i32), c as i32),
-            ),
-        ];
-        ConvexPolygon::new(projected_cube_vertices)
-    } else {
-        None
-    }
-}
-
-/// Partition the projected unit cube with the projected constraint planes.
-pub fn project_and_partition(a: u32, b: u32, c: u32) -> Vec<ConvexPolygon> {
-    let projected_cube = projected_cube(a, b, c).unwrap();
-    let lineses = projected_constraint_planes(a, b, c); // it's a vector of vector of `Line`s
-    let mut first_shapes = Vec::<ConvexPolygon>::new();
-    let mut second_shapes = Vec::<ConvexPolygon>::new();
-    let mut third_shapes = Vec::<ConvexPolygon>::new();
-    let mut i: usize = 0;
-    let mut remaining: ConvexPolygon = projected_cube;
-    loop {
-        let (maybe_remaining, maybe_next) = remaining.subdivide((lineses.0)[i]);
-        // The remaining portion is to the Left.
-        match (maybe_remaining, maybe_next) {
-            (Some(r), None) => {
-                remaining = r;
-            }
-            (Some(r), Some(next)) => {
-                remaining = r;
-                first_shapes.push(next);
-            }
-            (None, Some(next)) => {
-                // This is the last `next` you need to look at.
-                first_shapes.push(next);
-                break;
-            }
-            (_, _) => {
-                break;
-            }
-        }
-        i += 1;
-    }
-    for polygon in first_shapes {
-        let mut j: usize = 0;
-        let mut remaining2: ConvexPolygon = polygon;
-        loop {
-            let (maybe_remaining, maybe_next) = remaining2.subdivide((lineses.1)[j]); // The remaining portion is to the Left.
-            match (maybe_remaining, maybe_next) {
-                (Some(r), None) => {
-                    remaining2 = r;
-                }
-                (Some(r), Some(next)) => {
-                    remaining2 = r;
-                    second_shapes.push(next);
-                }
-                (None, Some(next)) => {
-                    second_shapes.push(next);
-                    break;
-                }
-                (_, _) => {
-                    break;
-                }
-            }
-            j += 1;
-        }
-    }
-    for polygon in second_shapes {
-        let mut j: usize = 0;
-        let mut remaining3: ConvexPolygon = polygon;
-        loop {
-            let (maybe_remaining, maybe_next) = remaining3.subdivide((lineses.2)[j]);
-            // The remaining portion is to the Left.
-            match (maybe_remaining, maybe_next) {
-                (Some(r), None) => {
-                    remaining3 = r;
-                }
-                (Some(r), Some(next)) => {
-                    remaining3 = r;
-                    third_shapes.push(next);
-                }
-                (None, Some(next)) => {
-                    third_shapes.push(next);
-                    break;
-                }
-                (_, _) => {
-                    break;
-                }
-            }
-            j += 1;
-        }
-    }
-    third_shapes
-}
-
-/// Advance a projected point, `pt`, by one letter according to what integer coordinate it next hits as it traverses in the given velocity,
-/// and return the next point and the corresponding letter, one of "x", "y", and "z".
-pub fn advance(a: u32, b: u32, c: u32, pt: Point) -> Result<(Point, Letter), String> {
-    let projected_cube = projected_cube(a, b, c).unwrap();
-    // Consider: _ /
-    //            |
-    if !projected_cube.has_point_inside(pt) {
-        Err(format!(
-            "Point passed to `advance`, {}, is not inside the projected cube",
-            pt
-        ))
-    } else {
-        let projected_x_y_equals_1_1 = Line::from_slope_and_point(
-            Slope::raw(b as i32, a as i32),
-            Point::new(r32::new(0, 1), r32::new(-(b as i32) + (a as i32), a as i32)),
-        ); // the /
-        let projected_x_z_equals_1_1 = Line::from_slope_and_point(
-            Slope::raw(0, 1),
-            Point::new(r32::new(0, 1), r32::new(-(b as i32) + (c as i32), c as i32)),
-        ); // the _, oriented right
-        let projected_y_z_equals_1_1 = Line::from_slope_and_point(
-            Slope::raw(1, 0),
-            Point::new(r32::new((c as i32) - (a as i32), c as i32), r32::new(0, 1)),
-        ); // the |, oriented up
-        if projected_x_y_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
-            && projected_y_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
-        {
-            // Below the / and to the right of the | => x has changed, change x coordinate to 0 and project; move x coordinate to the left
-            Ok((pt - Point::new(r32::new(1, 1), r32::new(0, 1)), 0)) // new point is one unit to the left
-        } else if projected_x_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
-            && projected_x_y_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
-        {
-            // Above the _ and to the left of the / => y has changed, change y coordinate to 0 and project; move y coordinate down
-            Ok((pt - Point::new(r32::new(0, 1), r32::new(1, 1)), 1)) // new point is one unit below
-        } else if projected_x_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Right
-            && projected_y_z_equals_1_1.point_line_config(pt) == PointLineConfiguration::Left
-        {
-            // Below the _ and to the left of the | => z has changed, change y coordinate to 0 and project
-            Ok((
-                pt + Point::new(r32::new(a as i32, c as i32), r32::new(b as i32, c as i32)),
-                2,
-            )) // new point is one unit below in the z-direction, and e3 is projected to (-a/c, -b/c)
-        } else {
-            Err(format!(
-                "Point passed to `advance`, {}, is inside the projected cube but falls on a projected constraint plane",
-                pt
-            ))
-        }
     }
 }
